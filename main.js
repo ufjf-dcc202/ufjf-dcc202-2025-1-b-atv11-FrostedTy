46493d77dace7a9b5f8b9b8242b807377ccfafdd
Gabriel Ernesto Leal de Bittencourt <gabobittencourt@gmail.com>
2025-07-20 21:56:35 -0300

    cria main

---
+++ main.js
@@ -0,0 +1,7 @@
+//main.js
+
+
+const olItens = document.querySelector("#itens");
+const pEntrada = document.querySelector('#entrada');
+const btnAdicionar = document.querySelector("#adicionar");
+const btnLimpar = document.querySelector("#limpar");
