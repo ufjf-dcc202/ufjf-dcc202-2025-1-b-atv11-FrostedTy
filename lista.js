ed9f7468d00ad02372e3804e2cfd8b0a267070bd
Gabriel Ernesto Leal de Bittencourt <gabobittencourt@gmail.com>
2025-07-20 23:46:10 -0300

    Modificações das estruturas.

--- lista.js
+++ lista.js
@@ -8,3 +8,8 @@ export function getLista() {
 export function limpaLista() {
   lista.splice(0);
 }
+
+export function adicionaNaLista(item){
+    lista.push(item.trim());
+}
+
--- main.js
+++ main.js
@@ -1,17 +1,31 @@
 //main.js
-
+import { adicionaNaLista, getLista, limpaLista } from "./lista.js";
 
 const olItens = document.querySelector("#itens");
-const pEntrada = document.querySelector('#entrada');
+const pEntrada = document.querySelector("#entrada");
 const btnAdicionar = document.querySelector("#adicionar");
 const btnLimpar = document.querySelector("#limpar");
 
+atualizarLista();
+btnLimpar.addEventListener("click", limparItensDeLista);
+
+btnAdicionar.addEventListener("click", () => {
+  adicionaNaLista(pEntrada.textContent);
+  atualizarLista();
+  pEntrada.textContent = ""; 
+});
+
+function limparItensDeLista() {
+  limpaLista();
+  atualizarLista();
+}
+
 function atualizarLista() {
-  olItems.innerHTML = "";
+  olItens.innerHTML = "";
   let lista = getLista();
   for (let i = 0; i < lista.length; i++) {
-    const li = document.createElement('li');
+    const li = document.createElement("li");
     li.textContent = lista[i];
-    olItems.appendChild(li);
+    olItens.appendChild(li);
   }
 }
